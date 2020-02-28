c9d8d5741204db961299c804af6da01a0d7acc2f
JohnerzZ <janniszar@gmail.com>
2020-02-28 13:59:44 +0200

    subtle changes

--- back-end/energy/api/routes/users.js
+++ back-end/energy/api/routes/users.js
@@ -9,15 +9,28 @@ var con = mysql.createConnection({
   database: "energymarket"
 });
 
+function randomStr(len, arr) { 
+            var ans = ''; 
+            for (var i = len; i > 0; i--) { 
+                ans +=  
+                  arr[Math.floor(Math.random() * arr.length)]; 
+            } 
+            return ans; 
+        } 
+
 router.post('/', (req, res, next) => {
    var username = req.body.username;
    var password = req.body.password;
    var quota = req.body.quota;
    var email = req.body.email;
-   var apikey = 133;
+   var apikey1 = randomStr(4,'12ab');
+   var apikey2 = randomStr(4, '32dc');
+   var apikey3 = randomStr(4, '54vg');
+   var dash = '-';
+   var apikey = apikey1.concat(dash, apikey2, dash, apikey3);
+   console.log(apikey);
   if (username && password) {
     con.query('INSERT INTO users (user_name, pass_word, email, quota, apikey) VALUES (?,?,?,?,?)', [username, password, email, quota, apikey], function(error, results, fields) {
-      //quotas sth bash
         res.status(200).json({
           message: 'User added successfuly.'
         })   
