98e66dc82681f6c2be7e72032109bc5a74204263
KonstantinosAnd <konstantinosand@yahoo.gr>
2020-02-29 13:42:09 +0200

    setup and executables fixed

--- cli-client/setup.py
+++ cli-client/setup.py
@@ -1,10 +1,16 @@
-import setuptools
+from setuptools import setup, find_packages
 
-setuptools.setup(
+setup(
     name="energy_group56", 
     version="0.0.1",
     author="Touloumbes",
     description="A CLI for Watterloo.",
     url="https://github.com/ntua/TL19-56/cli-client",
-    install_requires=['click', 'requests','datetime', 'click_option_group']
+    packages = find_packages(),
+    install_requires=['click', 'requests','datetime', 'click_option_group'],
+    entry_points={
+        'console_scripts': [ 
+            'energy_group56 = energy_group56.energy_group56:main'
+        ]
+    }
 )
