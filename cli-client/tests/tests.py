2a1cabf979b4c664c0b5678c5f8e44e73687f4a5
KonstantinosAnd <konstantinosand@yahoo.gr>
2020-03-03 01:51:56 +0200

    last few tests added

--- cli-client/tests/tests.py
+++ cli-client/tests/tests.py
@@ -1,20 +1,36 @@
 import os
-global count
+
 
 def test_basic_entrypoint():
     exit_status = os.system('energy_group56')
-    assert exit_status == 0
     print("Basic test was OK!")
+
 
 def test_login():
     exit_status = os.system('energy_group56 login')
     assert exit_status != 0
-    print("Login test was OK!")
+    print("Login asked for more parameters as expected!")
 
-def main():
-    test_basic_entrypoint()
-    test_login()
+
+def test_actualtotal():
+    exit_status = os.system('energy_group56 actualtotalload')
+    assert exit_status != 0
+    print("Actualtotalload energy-show command asked for more parameters as expected!")
+    
+
+def test_admin():
+    exit_status = os.system('energy_group56 admin')
+    assert exit_status != 0
+    print("Admin scope asked for more parameters as expected!")
+
 
 if __name__ == '__main__':
-    main()
+    test_basic_entrypoint()
+    print("TEST 1 WAS OK!")
+    test_login()
+    print("TEST 2 WAS OK!")
+    test_actualtotal()
+    print("TEST 3 WAS OK!")
+    test_admin()
+    print("TEST 4 WAS OK!")
     
